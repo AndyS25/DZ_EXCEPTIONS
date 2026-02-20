8a9ba2ec825ecacd82d670ebca385f18ff19e45b
Andrey Senkovich <senkovich@gmail.com>
2026-02-20 16:48:21 +0300

    Init commit

---
+++ src/main/java/ru/netology/javaqa/ShopRepositoryTest.java
@@ -0,0 +1,37 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class ShopRepositoryTest {
+    Product item1 = new Product(11,"хлеб", 40);
+    Product item2 = new Product(222,"булка", 30);
+    Product item3 = new Product(3, "картошка", 20);
+
+    ShopRepository repo = new ShopRepository();
+
+    @Test
+    public void shouldRemoveByIdDoesNotExist() {
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(1);
+        });
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+
+        repo.removeById(3);
+
+        Product[] expected = {item1, item2};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
